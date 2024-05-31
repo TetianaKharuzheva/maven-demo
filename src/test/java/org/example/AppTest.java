2d5f7e3c3e526f9a56d6e0fe904fdf0961fd7ba5
Тетяна <cerryz.ta.ha@gmail.com>
2024-05-31 15:01:07 +0300

    add dummy test

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,8 +1,16 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+    @Test
+    public void duummyTest() {
+        Assertions.assertTrue(true);
+
+    }
 
 }
